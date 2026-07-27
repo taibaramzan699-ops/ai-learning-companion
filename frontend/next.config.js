f8a9647cf190a912804e9a2827152ddbcc4c2d33
Taiba Ramzan <taibaramzan699@example.com>
2026-07-27 18:54:08 +0500

    Add fallback API URL

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,23 @@
 /** @type {import('next').NextConfig} */
+
+const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "res.cloudinary.com" },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
     ],
   },
   async rewrites() {
     return [
       {
         source: "/api/backend/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `${API_URL}/:path*`,
       },
     ];
   },
